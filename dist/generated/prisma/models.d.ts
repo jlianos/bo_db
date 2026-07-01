dd255471a60b97b95f140a69650a223e8211d13d
jlianos <J.lianos@futuresoft.gr>
2026-07-01 17:38:18 +0300

    Stop tracking dist

--- dist/generated/prisma/models.d.ts
+++
@@ -1,5 +0,0 @@
-export type * from './models/Menu.js';
-export type * from './models/MenuItem.js';
-export type * from './models/MenuItemPerMenu.js';
-export type * from './commonInputTypes.js';
-//# sourceMappingURL=models.d.ts.map
--- dist/generated/prisma/models/MenuItemPerMenu.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=MenuItemPerMenu.js.map
